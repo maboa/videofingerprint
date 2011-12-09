c180ecddba509172138a9fa105fb709426fb0f07
Mark Boas <markb@happyworm.com>
2011-12-10 00:44:57 +0100

    added more graphs

--- main.js
+++ main.js
@@ -18,14 +18,20 @@ var processor = {
     this.video = document.getElementById("video");
     this.c1 = document.getElementById("c1");
     this.fp = document.getElementById("fingerprint");
+    this.fc = document.getElementById("fingerrgb");
     this.dp = document.getElementById("deltaprint");
+    this.dt = document.getElementById("deltatotal");
+    this.dc = document.getElementById("deltargb");
     //this.c1 = document.createElement("c1");
     //this.c1.setAttribute('width',480);  
     //this.c1.setAttribute('height',270);
 
     this.ctx1 = this.c1.getContext("2d");
     this.ctxfp = this.fp.getContext("2d");
+    this.ctxfc = this.fc.getContext("2d");
     this.ctxdp = this.dp.getContext("2d");
+    this.ctxdt = this.dt.getContext("2d");
+    this.ctxdc = this.dc.getContext("2d");
 
     this.cb1 = document.getElementById("cb1");
     var self = this;
@@ -76,8 +82,29 @@ var processor = {
     this.ctxfp.strokeStyle = "rgb("+averageR+","+averageG+","+averageB+")"; 
     this.ctxfp.strokeRect(this.framenum,0,1,40);
 
+    this.ctxfc.strokeStyle = "rgb(255,0,0)"; 
+    this.ctxfc.strokeRect(this.framenum,averageR,1,1);
+    this.ctxfc.strokeStyle = "rgb(0,255,0)"; 
+    this.ctxfc.strokeRect(this.framenum,averageG,1,1);
+    this.ctxfc.strokeStyle = "rgb(0,0,255)"; 
+    this.ctxfc.strokeRect(this.framenum,averageB,1,1);
+
     this.ctxdp.strokeStyle = "rgb("+(255-deltaR)+","+(255-deltaG)+","+(255-deltaB)+")"; 
     this.ctxdp.strokeRect(this.framenum,0,1,40);
+
+    this.ctxdt.strokeStyle = "rgb(255,255,255)"; 
+    this.ctxdt.strokeRect(this.framenum,0,1,((deltaR+deltaG+deltaB)/3));
+
+    this.ctxdc.strokeStyle = "rgb(255,0,0)"; 
+    this.ctxdc.strokeRect(this.framenum,deltaR,1,1);
+    this.ctxdc.strokeStyle = "rgb(0,255,0)"; 
+    this.ctxdc.strokeRect(this.framenum,deltaG,1,1);
+    this.ctxdc.strokeStyle = "rgb(0,0,255)"; 
+    this.ctxdc.strokeRect(this.framenum,deltaB,1,1);
+
+    //white baseline
+    this.ctxdc.strokeStyle = "rgb(255,255,255)"; 
+    this.ctxdc.strokeRect(this.framenum,0,1,1);
 
     //document.body.style.backgroundColor="rgb("+averageR+","+averageG+","+averageB+")";
 
